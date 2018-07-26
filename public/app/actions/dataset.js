ec718c53f1741885aa6fe46658ac12150a38c313
Eric Youngberg <eyoungberg@mapc.org>
2018-07-26 08:34:10 -0400

    Fix disappearing datasets bug

--- public/app/actions/dataset.js
+++ public/app/actions/dataset.js
@@ -4,14 +4,17 @@ import types from './types';
 export function fetchAll() {
   return async (dispatch, getState) => {
     let datasets = getState().dataset.cache;
+    console.log(datasets);
 
     if (datasets.length === 0 ) {
       datasets = await (
         await fetch('https://datacommon.carto.mapc.org/api/v2/sql?q=select%20*%20from%20table_data_browser%20where%20schemaname%3D%27tabular%27%20or%20schemaname%3D%27mapc%27%20and%20active%3D%27Y%27')
       ).json();
+
+      datasets = datasets.rows;
     }
 
-    return dispatch(update(datasets.rows));
+    return dispatch(update(datasets));
   };
 }
 
