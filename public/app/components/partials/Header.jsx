951c54e2cf0610b5cced9a9d0897fcd9b2c376c3
Ian Hill <ihill@mapc.org>
2018-08-22 15:48:49 -0400

    Implement horizontal scrolling for mobile navigation

--- public/app/components/partials/Header.jsx
+++ public/app/components/partials/Header.jsx
@@ -8,8 +8,8 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <div className="container">
-          <nav>
+        <nav>
+          <div className="scroll-wrapper">
             <div className="header-brand">
               <a href="/">
                 <img src={logoImg} alt="DataCommon Logo" />
@@ -19,13 +19,11 @@ class Header extends React.Component {
 
             <ul>
               <li><a href="/browser">Datasets</a></li>
-              <li><a href="/community-profiles">Community Profiles</a></li>
-              <li><a href="/faq">FAQ</a></li>
+              <li><a href="/#community-profiles">Community Profiles</a></li>
             </ul>
-          </nav>
-
+          </div>
           <button className="login-button">Login</button>
-        </div>
+        </nav>
       </header>
     );
   }
