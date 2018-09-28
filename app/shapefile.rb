1980da94aacee91303656821fb55ebe3a626b5ed
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-09-28 17:35:24 -0400

    Fix Shapefile Download Link
    * Fix Shapefile Download Link

--- app/shapefile.rb
+++ app/shapefile.rb
@@ -52,8 +52,8 @@ module Shapefile
     end
 
     def response(request)
-      file = zip(to_shp(request.params['table']))
-      [200, {'Content-Type' => 'application/zip', 'Content-Disposition' => "attachment; filename=\"#{request.params['table']}\""}, FileStreamer.new("public/#{file}")]
+      file = zip(to_shp(request.params['table'],request.params['database']))
+      [200, {'Content-Type' => 'application/zip', 'Content-Disposition' => "attachment; filename=\"#{request.params['table']}.zip\""}, FileStreamer.new("public/#{file}")]
     end
 
     def call(env)
