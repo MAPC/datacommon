d2ea9249c1329c90626802bdbaec72d678b57701
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-11-13 11:44:53 -0500

    Add staging to babel

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
+  var validEnv = ['development', 'test', 'production', 'staging']
   var currentEnv = api.env()
   var isDevelopmentEnv = api.env('development')
   var isProductionEnv = api.env('production')
   var isTestEnv = api.env('test')
+  var isStagingEnv = api.env('staging')
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
