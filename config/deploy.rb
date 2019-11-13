040bdba1fbf236f343c13002fea639d63d912474
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-11-13 10:39:15 -0500

    Bump Ruby Version on Deploy
    Bump to 2.6.5 and fix ruby-version syntax.

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,4 +38,4 @@ append :linked_dirs, "log"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :rvm_ruby_version, '2.5.3'
+set :rvm_ruby_version, '2.6.5'
