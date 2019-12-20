5218fe9f9d57bc904dbb3940467d350a1d3159de
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-12-20 11:54:18 -0500

    Create temporary blank boxes for 2020
    * Will need to adjust when swapping from JSON backend to data model/API

---
+++ app/javascript/pages/components/partials/BlankCalendarItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class BlankCalendarItem extends React.Component {
+  render() {
+    return (
+      <li className="calendar-grid__cell">
+        <a href={this.props.link} className="calendar-item"> 
+          <div className="calendar-item__month">{this.props.month}</div>
+          <div className="calendar-item__box--blank">
+            <h3 className="calendar-item__title--blank">Coming Soon</h3>
+          </div>
+        </a>
+      </li>
+    );
+  }
+}
+  
+export default BlankCalendarItem;
+  
--- app/javascript/pages/components/partials/CalendarGrid.jsx
+++ app/javascript/pages/components/partials/CalendarGrid.jsx
@@ -1,24 +1,42 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import CalendarItem from './CalendarItem';
+import BlankCalendarItem from './BlankCalendarItem';
 import data from '../../assets/data/temp-cal-data.json';
 
 
 class CalendarGrid extends React.Component {
   render() {
-    const calendarItemsForYear = data.filter((item) => item.year === this.props.selectedYear)
-      .map((item, i) => (
-        <CalendarItem
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const existingCalendarItems = data.filter((item) => item.year === this.props.selectedYear)
+      .map((item, i) => {
+        months.shift();
+        return <CalendarItem
           link={item.url}
           month={item.month}
           image={item.image}
           title={item.title}
           key={i}
         />
-      ));
+      });
+
+    // Create some number of blank cards
+    const blankCalendarItems = months.map((month, i) => {
+      const blankDisplayMonth = `${month} '${this.props.selectedYear.toString().slice(-2)}`;
+      const keyValue = `b${i}`;
+      return <BlankCalendarItem
+        month={blankDisplayMonth}
+        key={keyValue}
+      />
+    })
+
+    const allCalendarItems = existingCalendarItems.concat(blankCalendarItems);
+  
+
 
     return (
       <ul className="component CalendarGrid container">
-        {calendarItemsForYear}
+        {allCalendarItems}
       </ul>
     );
   }
