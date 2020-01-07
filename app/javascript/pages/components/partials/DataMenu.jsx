2e812c062051ade16090a72c2ad20ba7c685f1d0
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-01-07 17:03:15 -0500

    Make Data Browser Mobile Tolerable
    Style changes impelemnted to make data browser mobile-tolerable.
    
    * Remove padding at smaller sizes.
    * Refactor DOM for DataMenu
    * Remove unnecessary CSS rule
    * Lower font-size of lbel and hide text of subtopic numbers to allow everything to fit on a mobile screen better.

--- app/javascript/pages/components/partials/DataMenu.jsx
+++ app/javascript/pages/components/partials/DataMenu.jsx
@@ -9,9 +9,7 @@ export default function DataMenu(props) {
           <li className='menu-item' onClick={item.items ? props.onMenuClick : props.onDatasetClick} key={index} data-key={item.dataset ? item.dataset.seq_id : index}>
             <div className="category-item-content">
               <div className="category-item-column">{item.menuTitle}</div>
-              <div className="category-item-column">
-                <small className="menu-item__dataset-count">{item.items ? item.items.length + ' Subtopics' : null}</small>
-              </div>
+              <div className="menu-item__dataset-count">{item.items ? item.items.length + ' Subtopics' : null}</div>
             </div>
           </li>
         ))}
