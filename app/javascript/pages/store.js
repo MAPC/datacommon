da15f8f97f69aea777dd98587db66946392f8072
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-01-16 13:40:59 -0500

    Add Google Analytics
    This commit adds Google Analytics to the app using the react-ga library and does so by connecting it to the Redux store.

--- app/javascript/pages/store.js
+++ app/javascript/pages/store.js
@@ -3,11 +3,26 @@ import { applyMiddleware, createStore } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import { createBrowserHistory } from 'history';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import ReactGA from 'react-ga';
 
 import rootReducer from './reducers/root';
 
+ReactGA.initialize('UA-5547782-35');
 
 export const history = createBrowserHistory();
+
+const trackPage = (page) => {
+  ReactGA.pageview(page);
+};
+
+const gaTrackingMiddleware = (store) => (next) => (action) => {
+  if (action.type === '@@router/LOCATION_CHANGE') {
+    const nextPage = `${action.payload.pathname}${action.payload.search}`;
+    trackPage(nextPage);
+  }
+  return next(action);
+};
+
 
 export const store = ((initialState, history) => (
   createStore(
@@ -15,9 +30,10 @@ export const store = ((initialState, history) => (
     initialState,
     composeWithDevTools(
       applyMiddleware(
+        gaTrackingMiddleware,
         middleware,
-        routerMiddleware(history)
-      )
-    )
+        routerMiddleware(history),
+      ),
+    ),
   )
 ))({}, history);
