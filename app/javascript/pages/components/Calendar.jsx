ffb0c088d0ef472f5eb14b867717fb6abd5e96a4
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-01-22 16:07:12 -0500

    Make Bar Chart Race Responsive
    * Refactor styling and positioning out of JavaScript into CSS.
    * Slight style tweaking for uniformity ok’d by Ryan.
    * Remove Dead code in CSS

--- app/javascript/pages/components/Calendar.jsx
+++ app/javascript/pages/components/Calendar.jsx
@@ -40,10 +40,14 @@ class Calendar extends React.Component {
       }
       const height = (barSize * topResults) + margin.top + margin.bottom;
 
-      const svg = d3.select('.jobs')
-        .attr('class', 'container')
+      let svg = d3.select('.jobs')
         .append('svg')
-        .attr('viewBox', [0, 0, width, height]);
+
+      if (width > 500) {
+        svg.attr('viewBox', [0, 0, width, height]);
+      } else {
+        svg.attr('viewBox', [0, 0, 700, 900]);
+      }
 
       const categories = new Set(filteredData.map((d) => d.category));
 
@@ -177,12 +181,6 @@ class Calendar extends React.Component {
         const now = d3.select('.jobs__subtitle')
           .append('div')
           .attr('class', 'jobs__year')
-          .style('font', `bold ${barSize}px var(--sans-serif)`)
-          .style('font-variant-numeric', 'tabular-nums')
-          .attr('text-anchor', 'end')
-          .attr('x', width - 6)
-          .attr('y', margin.top + barSize * (topResults - 0.45))
-          .attr('dy', '0.32em')
           .text(formatDate(allKeyframes[0][0]));
 
         d3.select('.jobs__subtitle')
@@ -197,10 +195,7 @@ class Calendar extends React.Component {
           .on('click', () => window.open('https://datacommon.mapc.org/browser/datasets/388'));
 
         return ([date], transition) => {
-          transition.end().then(() => now.text(formatDate(date))
-            .style('font-size', '68px')
-            .attr('fill-color', '#95989A')
-            .style('font-weight', 'bold'));
+          transition.end().then(() => now.text(formatDate(date)));
         };
       }
 
@@ -261,8 +256,8 @@ class Calendar extends React.Component {
           <a href="/gallery" className="back-link">{back}</a>
           <h1 className="jobs__title">Employment by Industry</h1>
           <div className="jobs" />
-          <h2 className="jobs__subtitle" />
-          <div className="container jobs__explanation">
+          <div className="jobs__subtitle" />
+          <div className="jobs__explanation">
             <p>Metro Boston has experienced rapid economic growth over the past two decades. Although the region experienced periods of employment decline during the 2001 and 2008 national recessions, its employment today is 10% above its previous peak set in 2001. Its top three employing industries since 2001—Professional and Technical Services, Educational Services, and Food Services and Drinking Places—together grew 38% from 2001 to 2018. Healthcare and social assistance industries account for a quarter of jobs among the region’s top 15 industries in 2018, and have seen 89% job growth since 2001. Jobs in Social Assistance, such as those in individual and family services and child day care, were the fastest growing among the region’s top 15, at 116%.</p>
             <p>Average wages in the region across all industries grew 15% (in real dollars) between 2001 and 2018. Yet, looking at wages in the region’s largest individual industries, wage growth has been polarized. Average weekly wages in Social Assistance—the top growing industry among the top 15—declined 4% from an already low $662 per week to $638, in 2018 dollars. Wages in Food Services and Drinking Places, the third largest industry and that with the lowest wages among the top 15, grew only 7%, from $457 to $489. On the other hand, wages in Professional and Technical Services grew 28%, from $2,163 to $2,774, and wages in Financial Investment and Related Activity, already the highest among the top 15, grew the fastest at 39%, from $4,520 to $6,305.</p>
             <p>The region’s prosperity depends not only on job growth and the continued competitiveness of the region’s economy, but also high quality of life for all its residents. With rising housing, transportation, healthcare, and childcare costs it is becoming even more difficult for low-wage employees to achieve this quality of life. While Massachusetts has increased the minimum wage in recent years, the state‘s policies should go further to ensure stability and prosperity for its lowest income workers, such as tying the minimum wage to inflation and requiring companies to pay tipped workers at least $15 an hour. Without ensuring stable, living wages for all employees, job growth in the region amounts to little.</p>
