05800313bd6fd582270bd8cb94ebb7210a410da5
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-02-28 15:41:54 -0500

    Fix gallery footer across browsers
    * The quickest way to ensure the download calendar CTA was consistent across all browsers was to make the image, overlay, and text one image. The font styling and positioning matches what was used prior, but this does mean that updating the font styles on the site (or the text used on the image) will require creating a new image.

--- app/javascript/pages/components/gallery/GalleryFooter.jsx
+++ app/javascript/pages/components/gallery/GalleryFooter.jsx
@@ -5,13 +5,10 @@ import CalendarImg from '../../assets/images/calendar.png';
 const GalleryFooter = () => (
   <div className="gallery-footer">
     <div className="gallery-footer__content">
-      <a href="/files/final-mapc-calendar-2020.pdf">
-        <div className="calendar-download">
-          <img src={CalendarImg} alt="MAPC 2020 calendar" className="calendar-download__image" />
-          <h3 className="calendar-download__text">Download 2020 Wall Calendar</h3>
-        </div>
-        <div className="calendar-download__calendar-shadow" />
+      <a href="/files/final-mapc-calendar-2020.pdf" className="calendar-download">
+        <img src={CalendarImg} className="calendar-download__image" alt="2020 MAPC calendar (Select to download)" />
       </a>
+      <div className="calendar-download__calendar-shadow" />
       <div className="gallery-footer__info">
         <h2 className="gallery-footer__title">2020 Calendar</h2>
         <p>MAPC has for many years produced a wall calendar that provides recipients with insightful information about the region, in the form of a monthly map or data visualization. Each year, these monthly segments covered a wide variety of topics.</p>
