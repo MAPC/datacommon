22fc325814cca171475b6df580d21e805100a76c
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-02-28 11:30:03 -0500

    Left align header on IE

--- app/javascript/pages/components/partials/Header.jsx
+++ app/javascript/pages/components/partials/Header.jsx
@@ -9,25 +9,23 @@ class Header extends React.Component {
     const highlightCPLink = this.props.location.pathname.startsWith('/profile');
 
     return (
-      <header>
-        <div className="container">
-          <nav>
-            <div className="scroll-wrapper">
-              <div className="header-brand">
-                <a href="/">
-                  <img src={logoImg} alt="DataCommon Logo" />
-                  DataCommon
-                </a>
-              </div>
-
-              <ul>
-                <li><a href="/browser">Datasets</a></li>
-                <li><a className={highlightCPLink ? 'active' : null} href="/#community-profiles">Community Profiles</a></li>
-                <li><a href="/gallery">Gallery</a></li>
-              </ul>
+      <header className="container">
+        <nav>
+          <div className="scroll-wrapper">
+            <div className="header-brand">
+              <a href="/">
+                <img src={logoImg} alt="DataCommon Logo" />
+                DataCommon
+              </a>
             </div>
-          </nav>
-        </div>
+
+            <ul>
+              <li><a href="/browser">Datasets</a></li>
+              <li><a className={highlightCPLink ? 'active' : null} href="/#community-profiles">Community Profiles</a></li>
+              <li><a href="/gallery">Gallery</a></li>
+            </ul>
+          </div>
+        </nav>
       </header>
     );
   }
