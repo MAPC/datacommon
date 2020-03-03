cc8be2556ad97e6289a154353c821f3079912f53
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-03-03 10:47:47 -0500

    Refactor SCSS before merging branch
    * In order to follow BEM rules (and because it was already moved into a separate component), I moved calendar-box__xyz--blank rules into a new blank-calendar-box__xyz class set

--- app/javascript/pages/components/gallery/BlankCalendarItem.jsx
+++ app/javascript/pages/components/gallery/BlankCalendarItem.jsx
@@ -4,11 +4,9 @@ import PropTypes from 'prop-types';
 const BlankCalendarItem = (props) => {
   const { month } = props;
   return (
-    <li className="calendar-item__wrapper">
+    <li className="calendar-item__wrapper--blank">
       <h2 className="calendar-item__month">{month}</h2>
-      <div className="calendar-item__box--blank">
-        <h3 className="calendar-item__title--blank">Coming Soon</h3>
-      </div>
+      <h3 className="blank-calendar-item__title">Coming Soon</h3>
     </li>
   );
 };
