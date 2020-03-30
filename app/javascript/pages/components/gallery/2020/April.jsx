84fc9cb2134bcf633bc21051f448bd464787be93
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-03-30 11:15:47 -0400

    Update map hatching, calendar images, narrative, title

--- app/javascript/pages/components/gallery/2020/April.jsx
+++ app/javascript/pages/components/gallery/2020/April.jsx
@@ -105,9 +105,9 @@ const April = () => {
         );
         const tractId = clickedData[0].properties.ct10_id;
         const tractData = percentageCompOwnership[tractId] <= 100
-          ? `${d3.format('.1f')(percentageCompOwnership[tractId])}% (&#177; ${compMarginOfError[tractId]}%) of approx. ${d3.format(',')(numHouseholds[tractId])} households`
+          ? `${d3.format('.1f')(percentageCompOwnership[tractId])}% (&#177; ${compMarginOfError[tractId]}%) of approximately ${d3.format(',')(numHouseholds[tractId])} households lack a computing device`
           : 'Computer ownership data unavailable';
-        const returnRate = +responseRates[tractId] !== 99999.0 ? `${responseRates[tractId]}% 2010 census mail return rate` : '2010 census return rate unavailable'
+        const returnRate = +responseRates[tractId] !== 99999.0 ? `${responseRates[tractId]}% 2010 census mail return rate` : '2010 census return rate unavailable';
         const tooltipText = `<p class='tooltip__title'>Tract ${tractId}
         (${clickedData[2].properties.municipal})</p>
         <ul class='tooltip__list'>
@@ -123,12 +123,12 @@ const April = () => {
   });
   return (
     <>
-      <h1 className="calendar-viz__title">The Digital Census</h1>
+      <h1 className="calendar-viz__title">The Digital Divide and the 2020 Census</h1>
       <div className="calendar-viz__wrapper">
         <div id="aprilMap" className="mapboxgl__container" />
-        <div className="map__overlay">
-          <span className="map__legend-entry map__legend-entry--bold map__legend-title">Households without a computer</span>
-          <svg height="190" width="160" className="map__legend map__legend--translucent">
+        <div className="map__overlay" style={{ top: '98px' }}>
+          <span className="map__legend-entry map__legend-entry--bold map__legend-title">Households without a computing device</span>
+          <svg height="258" width="160" className="map__legend map__legend--translucent">
             <rect x="10" y="2" width="16" height="16" style={{ fill: colors[0], stroke: 'black', strokeWidth: '1px' }} />
             <text x="32" y="14" className="map__legend-entry" fill="#1F4E46">25 – 35%</text>
             <rect x="10" y="30" width="16" height="16" style={{ fill: colors[1], stroke: 'black', strokeWidth: '1px' }} />
@@ -146,13 +146,31 @@ const April = () => {
             <text x="32" y="154" className="map__legend-entry" fill="#1F4E46">Hard-to-count tract</text>
             <text x="32" y="166" className="map__legend-entry" fill="#1F4E46">(&#x2264; 73% mail return</text>
             <text x="32" y="178" className="map__legend-entry" fill="#1F4E46">rate, 2010 census)</text>
+            <text x="8" y="198" className="map__legend-entry map__legend-entry--bold" fill="#1F4E46">Explore & Download</text>
+            <text x="8" y="212" className="map__legend-entry map__legend-entry--bold" fill="#1F4E46">Data</text>
+            <text x="8" y="230" className="map__legend-entry" fill="#1F4E46">
+              &#8226;
+              {' '}
+              <a href="https://www.censushardtocountmaps2020.us/" className="calendar-viz__link" fill="#1F4E46">Census mail return rate</a>
+            </text>
+            <text x="8" y="246" className="map__legend-entry" fill="#1F4E46">
+              &#8226;
+              {' '}
+              <a href="https://data.census.gov/cedsci/map?q=massachusetts&g=0400000US25.140000,25&hidePreview=false&tid=ACSST5Y2018.S2801&vintage=2018&cid=S2801_C01_001E&t=Telephone,%20Computer,%20and%20Internet%20Access" className="calendar-viz__link" fill="#1F4E46">2014⁠–2018 ACS
+              </a>
+            </text>
           </svg>
         </div>
       </div>
       <p>The COVID-19 pandemic has upended daily life in Greater Boston and around the world. Thousands of people can’t report to work, and millions are being told to cease nonessential travel. Never before has the internet been so essential for working remotely and staying connected. Unfortunately, 10 percent of the region’s households still lack the technology needed to access the internet from home. Not only does this inequity make it harder for people to get the information and socialization they need to make it through this crisis; it will also impact the decennial US census being conducted this month.</p>
       <p>While the census might seem extraneous to some people right now, it is of utmost importance to the functioning of government, including preparation for future public emergencies. Census counts determine legislative apportionment and districts; are the basis for federal funding allocation; and inform innumerable policy, planning, emergency response, and business decisions.</p>
-      <p>Even with this year’s new option to answer the census online, getting every household to respond is a tough job. This is especially true in <a href="https://www.bostonindicators.org/reports/report-website-pages/census-2020" className="calendar-viz__link">“hard-to-count”</a> communities where limited English proficiency, immigration status, nontraditional housing arrangements, and distrust of government, among other factors, are formidable barriers to getting a complete count. Many municipalities and community-based organizations had planned extensive personal outreach—door knocking, parties, census kiosks—to promote census response in these neighborhoods. However, the current need for “social distancing” means that all of these in-person efforts have been cancelled.</p>
-      <p>Like many jobs, census outreach and follow-up efforts can’t all be shifted online. As shown on the map, hard-to-count tracts often correspond to those lacking the technology needed to access the internet. In some neighborhoods, over 30 percent of households do not have so much as a smartphone. This will make it hard to reach households with online advertising promoting census response, and these same households will have to wait for a paper form rather than responding online. The digital divide may depress return rates among the most at-risk communities, with long term effects on funding, services, and representation.</p>
+      <p>Even with this year’s new option to answer the census online, getting every household to respond is a tough job. This is especially true in
+      {' '}
+        <a href="https://www.bostonindicators.org/reports/report-website-pages/census-2020" className="calendar-viz__link">“hard-to-count”</a>
+        {' '}
+      communities where limited English proficiency, immigration status, nontraditional housing arrangements, and distrust of government, among other factors, are formidable barriers to getting a complete count. Many municipalities and community-based organizations had planned extensive personal outreach—tabling, fairs and parties, and question assistance centers—to promote census response in these neighborhoods. However, the current need for “social distancing” means that all of these in-person efforts have been cancelled.
+      </p>
+      <p>Like many jobs, census outreach and follow-up efforts can’t all be shifted online. As shown on the map, hard-to-count tracts often correspond to those lacking the technology needed to access the internet. In some neighborhoods, over 30 percent of households do not have a computer, smartphone, tablet, or some other computing device. This will make it hard to reach households with online advertising promoting census response, and these same households will have to wait for a paper form rather than responding online. The digital divide may depress return rates among the most at-risk communities, with long term effects on funding, services, and representation.</p>
       <p>It’s more critical than ever to respond promptly to the census so that follow-up efforts can be focused on communities that need it. As the current pandemic accelerates our transformation into a more digital region, we must pay attention to equity of access to the online world.</p>
     </>
   );
