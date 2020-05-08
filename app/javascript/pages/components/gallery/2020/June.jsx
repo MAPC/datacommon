2493e8cba5eca463aeb36187048dd27b3e084cbb
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-05-08 11:21:54 -0400

    Add tooltip to June visualization

--- app/javascript/pages/components/gallery/2020/June.jsx
+++ app/javascript/pages/components/gallery/2020/June.jsx
@@ -2,149 +2,22 @@
 import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
 
+function displayToolTip(data) {
+  return `<p class="tooltip__title">${data['Occupation (shortened)']} Occupations</p>
+    <ul class='tooltip__list'>
+      <li class='tooltip__text'>Median weekly salary: ${d3.format('$,.2f')(data.pre_median / 52)}</li>
+      <li class='tooltip__text'>Median weekly unemployment benefit: ${d3.format('$,.2f')(data.post_median / 52)}</li>
+    </ul>`;
+}
 
-const June = () => {
-  const [employmentData, setData] = useState([]);
-  const svgWidth = 1000 || window.innerWidth;
-  const svgHeight = 625;
-  const yAxisOffset = 300;
-  useEffect(() => {
-    d3.csv('/assets/june2020.csv').then((result) => {
-      setData(result);
-    });
-  }, []);
+function tooltipLeft(event, tooltip) {
+  // todo: switch tooltip to display to left of mouse on the right of the dividing line
+  return `${event.clientX - tooltip.offsetWidth + 20}px`;
+}
 
-  const yAxis = d3.scaleBand()
-    .range([0, svgHeight - 20])
-    .padding(1)
-    .domain(employmentData.map((d) => d['Occupation (shortened)']));
-
-  const xAxis = d3.scaleLinear()
-    .range([svgWidth, 0])
-    .domain([3400, 0]);
-
-  if (document.querySelector('svg') === null) {
-    const svg = d3.select('.calendar-viz')
-      .append('svg')
-      .attr('width', '100%')
-      .attr('height', svgHeight);
-
-    const graph = svg.append('g')
-      .attr('transform', `translate(${yAxisOffset + 12},${24})`)
-      .attr('class', 'graph');
-
-    graph.append('g')
-      .call(d3.axisTop(xAxis)
-        .tickFormat(d3.format('$~s')))
-      .attr('class', 'xaxis')
-      .style('font-size', '12px')
-      .style('font-variant-numeric', 'tabular-nums')
-      .style('font-family', 'Montserrat')
-      .attr('text-anchor', 'middle');
-
-    graph.append('g')
-      .call(d3.axisLeft(yAxis))
-      .attr('class', 'yaxis')
-      .style('font-size', '12px')
-      .style('font-variant-numeric', 'tabular-nums')
-      .style('font-family', 'Montserrat')
-      .attr('text-anchor', 'end')
-      .selectAll('text')
-      .call(wrap, yAxisOffset)
-      .attr('dx', '-12px');
-
-    graph.append('line')
-      .style('stroke', '#1F4E46')
-      .style('opacity', 0.3)
-      .style('stroke-width', 1)
-      .style('stroke-dasharray', '5, 5')
-      .attr('x1', xAxis(1423))
-      .attr('y1', 0)
-      .attr('x2', xAxis(1423))
-      .attr('y2', svgHeight);
-
-    graph.append('g')
-      .selectAll('pre-ranges')
-      .data(employmentData)
-      .enter()
-      .append('rect')
-      .style('fill', '#FFEB7F')
-      .attr('x', (d) => xAxis(+d.pre_firstquartile / 52))
-      .attr('y', (d) => yAxis(d['Occupation (shortened)']) - 5)
-      .attr('width', (d) => xAxis(+d.pre_thirdquartile / 52) - xAxis(+d.pre_firstquartile / 52))
-      .attr('height', 10);
-
-    graph.append('g')
-      .selectAll('post-ranges')
-      .data(employmentData)
-      .enter()
-      .append('rect')
-      .style('fill', 'cornflowerblue')
-      .style('opacity', 0.5)
-      .attr('x', (d) => xAxis(+d.post_firstquartile / 52))
-      .attr('y', (d) => yAxis(d['Occupation (shortened)']) - 5)
-      .attr('width', (d) => xAxis(+d.post_thirdquartile / 52) - xAxis(+d.post_firstquartile / 52))
-      .attr('height', 10);
-
-    graph.append('g')
-      .selectAll('pre-medians')
-      .data(employmentData)
-      .enter()
-      .append('line')
-      .style('stroke', 'gold')
-      .style('stroke-width', 3)
-      .attr('x1', (d) => xAxis(+d.pre_median / 52))
-      .attr('y1', (d) => yAxis(d['Occupation (shortened)']) + 10)
-      .attr('x2', (d) => xAxis(+d.pre_median / 52))
-      .attr('y2', (d) => yAxis(d['Occupation (shortened)']) - 10);
-
-    graph.append('g')
-      .selectAll('post-medians')
-      .data(employmentData)
-      .enter()
-      .append('line')
-      .style('stroke', 'cornflowerblue')
-      .style('stroke-width', 3)
-      .attr('x1', (d) => xAxis(+d.post_median / 52))
-      .attr('y1', (d) => yAxis(d['Occupation (shortened)']) + 10)
-      .attr('x2', (d) => xAxis(+d.post_median / 52))
-      .attr('y2', (d) => yAxis(d['Occupation (shortened)']) - 10);
-
-    graph.append('g')
-      .selectAll('connections')
-      .data(employmentData)
-      .enter()
-      .append('line')
-      .style('stroke', '#1F4E46')
-      .style('opacity', 0.5)
-      .style('stroke-width', 1)
-      .attr('x1', (d) => {
-        if (+d.pre_thirdquartile < +d.post_firstquartile) {
-          return xAxis(+d.pre_thirdquartile / 52);
-        }
-        return 0;
-      })
-      .attr('y1', (d) => yAxis(d['Occupation (shortened)']))
-      .attr('x2', (d) => {
-        if (+d.pre_thirdquartile < +d.post_firstquartile) {
-          return xAxis(+d.post_firstquartile / 52);
-        }
-        return 0;
-      })
-      .attr('y2', (d) => yAxis(d['Occupation (shortened)']));
-  }
-
-  return (
-    <>
-      <h1 className="calendar-viz__title">Untitled Unemployment Visualization</h1>
-      <div className="calendar-viz container" />
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit fermentum est, nec rhoncus massa rhoncus ut. Praesent fringilla tellus sed mi condimentum dignissim. Aliquam sagittis, neque sed feugiat fermentum, elit turpis scelerisque tortor, id venenatis dui eros quis urna. Praesent sagittis justo ac sem auctor tincidunt. Duis rhoncus condimentum mauris a laoreet. Sed euismod volutpat aliquam. Aliquam porta tortor id ligula sollicitudin, in pulvinar magna pretium. Duis tristique volutpat ante, vitae mattis urna vestibulum sit amet. Donec urna elit, iaculis et cursus nec, auctor ac felis. Donec consequat neque quam, nec euismod libero iaculis nec. Duis non neque tellus. Mauris aliquet mauris in fermentum malesuada. Fusce lacinia aliquam ullamcorper. Proin ut justo in ex mollis lobortis blandit sed ante. Pellentesque suscipit nulla ac congue pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-      <p>Pellentesque egestas ut justo quis dignissim. Nam scelerisque tristique pulvinar. Ut gravida, eros vitae sollicitudin maximus, odio orci finibus augue, congue consectetur arcu purus vitae odio. Integer non nibh id tortor dignissim interdum. Etiam iaculis quam ut ultrices lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec libero enim, pulvinar quis tincidunt at, tempor et sapien. Ut tempor magna nibh, vitae egestas justo semper bibendum. Praesent pellentesque semper mauris, a consectetur augue eleifend efficitur. Etiam rhoncus rutrum libero, eleifend varius nulla consequat id. Suspendisse nisi metus, luctus vitae sollicitudin vel, tristique at enim. Suspendisse gravida orci in nisl ultrices sagittis. Phasellus felis risus, venenatis sed dolor sed, consectetur suscipit nunc.</p>
-      <p>Curabitur consequat nibh et vulputate vulputate. Pellentesque iaculis dolor eget aliquam dignissim. Mauris ut lectus quis turpis iaculis blandit quis eu justo. In at sollicitudin augue. Nullam fringilla tortor vel lacus tincidunt bibendum. Donec ac tellus id nibh posuere eleifend. Cras suscipit nisi ut justo tincidunt placerat. Maecenas efficitur vestibulum ex, ut iaculis nibh. Morbi ac turpis a nisi mollis vestibulum id ac nulla. Sed posuere orci quis eleifend tempor.</p>
-      <p>Phasellus rutrum augue nisl, non euismod arcu mattis nec. Cras lacinia quam vestibulum quam ultricies malesuada. Nam ultrices dolor mauris, sit amet placerat ipsum porttitor.</p>
-    </>
-  );
-};
+function tooltipTop(event, tooltip) {
+  return `${event.pageY - tooltip.offsetHeight - 10}px`;
+}
 
 function wrap(text, width) {
   text.each(function () {
@@ -173,5 +46,145 @@ function wrap(text, width) {
     }
   });
 }
+
+const June = () => {
+  const [employmentData, setData] = useState([]);
+  const svgWidth = 1000 || window.innerWidth;
+  const svgHeight = 650;
+  const yAxisOffset = 300;
+  useEffect(() => {
+    d3.csv('/assets/june2020.csv').then((result) => {
+      setData(result);
+    });
+  }, []);
+
+  const yAxis = d3.scaleBand()
+    .range([0, svgHeight - 20])
+    .padding(1)
+    .domain(employmentData.map((d) => d['Occupation (shortened)']));
+
+  const xAxis = d3.scaleLinear()
+    .range([svgWidth, 0])
+    .domain([3400, 0]);
+
+  if (document.querySelector('svg') === null) {
+    const svg = d3.select('.calendar-viz')
+      .append('svg')
+      .attr('width', '100%')
+      .attr('height', svgHeight);
+
+    const tooltip = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('display', 'none');
+
+    const graph = svg.append('g')
+      .attr('transform', `translate(${yAxisOffset + 12},${24})`)
+      .attr('class', 'graph');
+
+    graph.append('g')
+      .call(d3.axisTop(xAxis)
+        .tickFormat(d3.format('$~s')))
+      .attr('class', 'xaxis')
+      .style('font-size', '12px')
+      .style('font-variant-numeric', 'tabular-nums')
+      .style('font-family', 'Montserrat')
+      .attr('text-anchor', 'middle');
+
+    graph.append('g')
+      .call(d3.axisLeft(yAxis))
+      .attr('class', 'yaxis')
+      .style('font-size', '12px')
+      .style('font-variant-numeric', 'tabular-nums')
+      .style('font-family', 'Montserrat')
+      .attr('text-anchor', 'end')
+      .selectAll('text')
+      .call(wrap, yAxisOffset)
+      .attr('dx', '-12px');
+
+    console.log(xAxis(1423))
+    graph.append('line')
+      .style('stroke', '#1F4E46')
+      .style('opacity', 0.3)
+      .style('stroke-width', 1)
+      .style('stroke-dasharray', '5, 5')
+      .attr('x1', xAxis(1423))
+      .attr('y1', 0)
+      .attr('x2', xAxis(1423))
+      .attr('y2', svgHeight);
+
+    employmentData.forEach((occupation) => {
+      const barGroup = graph.append('g').attr('class', `soc__${occupation.soc_2}`);
+
+      barGroup.append('rect')
+        .style('fill', '#FFEB7F')
+        .attr('x', xAxis(+occupation.pre_firstquartile / 52))
+        .attr('y', yAxis(occupation['Occupation (shortened)']) - 5)
+        .attr('width', xAxis(+occupation.pre_thirdquartile / 52) - xAxis(+occupation.pre_firstquartile / 52))
+        .attr('height', 10);
+
+      barGroup.append('rect')
+        .style('fill', 'cornflowerblue')
+        .style('opacity', 0.5)
+        .attr('x', xAxis(+occupation.post_firstquartile / 52))
+        .attr('y', yAxis(occupation['Occupation (shortened)']) - 5)
+        .attr('width', xAxis(+occupation.post_thirdquartile / 52) - xAxis(+occupation.post_firstquartile / 52))
+        .attr('height', 10);
+
+      barGroup.append('line')
+        .style('stroke', 'gold')
+        .style('stroke-width', 3)
+        .attr('x1', xAxis(+occupation.pre_median / 52))
+        .attr('y1', yAxis(occupation['Occupation (shortened)']) + 10)
+        .attr('x2', xAxis(+occupation.pre_median / 52))
+        .attr('y2', yAxis(occupation['Occupation (shortened)']) - 10);
+
+      barGroup.append('line')
+        .style('stroke', 'cornflowerblue')
+        .style('stroke-width', 3)
+        .attr('x1', xAxis(+occupation.post_median / 52))
+        .attr('y1', yAxis(occupation['Occupation (shortened)']) + 10)
+        .attr('x2', xAxis(+occupation.post_median / 52))
+        .attr('y2', yAxis(occupation['Occupation (shortened)']) - 10);
+
+      barGroup.append('line')
+        .style('stroke', '#1F4E46')
+        .style('opacity', 0.5)
+        .style('stroke-width', 1)
+        .attr('x1', (d) => {
+          if (+occupation.pre_thirdquartile < +occupation.post_firstquartile) {
+            return xAxis(+occupation.pre_thirdquartile / 52);
+          }
+          return 0;
+        })
+        .attr('y1', yAxis(occupation['Occupation (shortened)']))
+        .attr('x2', () => {
+          if (+occupation.pre_thirdquartile < +occupation.post_firstquartile) {
+            return xAxis(+occupation.post_firstquartile / 52);
+          }
+          return 0;
+        })
+        .attr('y2', yAxis(occupation['Occupation (shortened)']));
+
+      barGroup.on('mousemove', () => {
+        tooltip.html(displayToolTip(occupation));
+        tooltip.style('display', 'inline')
+          .style('left', tooltipLeft(d3.event, document.getElementsByClassName('tooltip')[0]))
+          .style('top', tooltipTop(d3.event, document.getElementsByClassName('tooltip')[0]));
+      })
+        .on('mouseleave', () => { tooltip.style('display', 'none'); });
+    });
+  }
+
+  return (
+    <>
+      <h1 className="calendar-viz__title">Untitled Unemployment Visualization</h1>
+      <div className="calendar-viz container" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit fermentum est, nec rhoncus massa rhoncus ut. Praesent fringilla tellus sed mi condimentum dignissim. Aliquam sagittis, neque sed feugiat fermentum, elit turpis scelerisque tortor, id venenatis dui eros quis urna. Praesent sagittis justo ac sem auctor tincidunt. Duis rhoncus condimentum mauris a laoreet. Sed euismod volutpat aliquam. Aliquam porta tortor id ligula sollicitudin, in pulvinar magna pretium. Duis tristique volutpat ante, vitae mattis urna vestibulum sit amet. Donec urna elit, iaculis et cursus nec, auctor ac felis. Donec consequat neque quam, nec euismod libero iaculis nec. Duis non neque tellus. Mauris aliquet mauris in fermentum malesuada. Fusce lacinia aliquam ullamcorper. Proin ut justo in ex mollis lobortis blandit sed ante. Pellentesque suscipit nulla ac congue pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+      <p>Pellentesque egestas ut justo quis dignissim. Nam scelerisque tristique pulvinar. Ut gravida, eros vitae sollicitudin maximus, odio orci finibus augue, congue consectetur arcu purus vitae odio. Integer non nibh id tortor dignissim interdum. Etiam iaculis quam ut ultrices lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec libero enim, pulvinar quis tincidunt at, tempor et sapien. Ut tempor magna nibh, vitae egestas justo semper bibendum. Praesent pellentesque semper mauris, a consectetur augue eleifend efficitur. Etiam rhoncus rutrum libero, eleifend varius nulla consequat id. Suspendisse nisi metus, luctus vitae sollicitudin vel, tristique at enim. Suspendisse gravida orci in nisl ultrices sagittis. Phasellus felis risus, venenatis sed dolor sed, consectetur suscipit nunc.</p>
+      <p>Curabitur consequat nibh et vulputate vulputate. Pellentesque iaculis dolor eget aliquam dignissim. Mauris ut lectus quis turpis iaculis blandit quis eu justo. In at sollicitudin augue. Nullam fringilla tortor vel lacus tincidunt bibendum. Donec ac tellus id nibh posuere eleifend. Cras suscipit nisi ut justo tincidunt placerat. Maecenas efficitur vestibulum ex, ut iaculis nibh. Morbi ac turpis a nisi mollis vestibulum id ac nulla. Sed posuere orci quis eleifend tempor.</p>
+      <p>Phasellus rutrum augue nisl, non euismod arcu mattis nec. Cras lacinia quam vestibulum quam ultricies malesuada. Nam ultrices dolor mauris, sit amet placerat ipsum porttitor.</p>
+    </>
+  );
+};
 
 export default June;
