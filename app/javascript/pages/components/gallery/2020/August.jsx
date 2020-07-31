921a3c15e2b4ac34c9e492b8a4ba359d048a447f
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-07-31 14:55:45 -0400

    Update calendar test suite

--- app/javascript/pages/components/gallery/2020/August.jsx
+++ app/javascript/pages/components/gallery/2020/August.jsx
@@ -6,7 +6,7 @@ const August = () => (
   <>
     <h1 className="calendar-viz__title">Paycheck Protection Program</h1>
     <div className="calendar-viz__iframe-wrapper">
-      <iframe src="https://mapc.github.io/MapboxEmbeds/2020/07/15/ppp.html" className="calendar-viz__iframe" id="july-iframe" title="Map of PPI scores across region" />
+      <iframe src="https://mapc.github.io/MapboxEmbeds/2020/07/15/ppp.html" className="calendar-viz__iframe" id="august-iframe" title="Map of PPI scores across region" />
     </div>
     <p>Pellentesque imperdiet felis ut nisi dictum, at gravida dolor vehicula. Proin sit amet turpis accumsan, varius lacus id, feugiat elit. Nunc eu lectus ut libero tincidunt gravida. Nullam tristique, lorem ut varius tincidunt, est tellus interdum orci, nec imperdiet eros mi vitae justo. In et lobortis mauris, pellentesque cursus mauris.</p>
     <p>Pellentesque imperdiet felis ut nisi dictum, at gravida dolor vehicula. Proin sit amet turpis accumsan, varius lacus id, feugiat elit. Nunc eu lectus ut libero tincidunt gravida. Nullam tristique, lorem ut varius tincidunt, est tellus interdum orci, nec imperdiet eros mi vitae justo. In et lobortis mauris, pellentesque cursus mauris.</p>
