11a5dd3d17a72248464adb31a24caa66ebfcd208
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-16 13:03:37 -0400

    Return all Massachusetts dogs in Ruby route

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,12 +5,22 @@ require 'petfinder'
 
 class CalendarController < ApplicationController
   def dogs
+    allDogs = []
+    currentPage = 1
     petfinder = Petfinder::Client.new(ENV.fetch("PETFINDER_API_KEY"), ENV.fetch("PETFINDER_SECRET"))
-    animals, pagination = petfinder.animals(type: 'dog', location: 'MA', page: 1, limit: 100)
-    # puts animals
-    puts pagination.total_pages
+    animals, pagination = petfinder.animals(type: 'dog', location: 'MA', page: currentPage, limit: 100)
+    allDogs.push(*animals)
+    
+    loop do
+      currentPage += 1
+      nextAnimal, nextPagination = petfinder.animals(type: 'dog', location: 'MA', page: currentPage, limit: 100)
+      allDogs.push(*nextAnimal)
+      if currentPage > pagination.total_pages
+        break
+      end
+    end
 
     # Return JSON object
-    render json: animals
+    render json: allDogs
   end
 end
