1d8e4db8b6cee817a16abe6e9ffbc55b7c1d5689
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-16 12:25:03 -0400

    Use Petfinder Ruby API wrapper for requests

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem "aws-sdk-s3", require: false
 gem "mini_magick"
 gem "non-digest-assets"
 
+
+gem 'petfinder'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,15 +1,16 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'petfinder'
 
 class CalendarController < ApplicationController
   def dogs
-    tokenUri = URI('https://api.petfinder.com/v2/oauth2/token')
-    tokenRes = Net::HTTP.post_form(tokenUri, 'grant_type' => 'client_credentials', 'client_id' =>  ENV.fetch("PETFINDER_API_KEY"), 'client_secret' => ENV.fetch("PETFINDER_SECRET"))
-    token = ActiveSupport::JSON.decode(tokenRes.body)["access_token"]
-    puts token
+    petfinder = Petfinder::Client.new(ENV.fetch("PETFINDER_API_KEY"), ENV.fetch("PETFINDER_SECRET"))
+    animals, pagination = petfinder.animals(type: 'dog', location: 'MA', page: 1, limit: 100)
+    # puts animals
+    puts pagination.total_pages
 
     # Return JSON object
-    render json: {'Fido': 'An ideal companion'}
+    render json: animals
   end
 end
