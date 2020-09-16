745635feecc0e3892bbd832e42599adea68fc0db
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-16 11:43:34 -0400

    Save authorization token

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,10 +1,13 @@
 require 'net/http'
+require 'uri'
+require 'json'
 
 class CalendarController < ApplicationController
   def dogs
     tokenUri = URI('https://api.petfinder.com/v2/oauth2/token')
-    res = Net::HTTP.post_form(tokenUri, 'grant_type' => 'client_credentials', 'client_id' =>  ENV.fetch("PETFINDER_API_KEY"), 'client_secret' => ENV.fetch("PETFINDER_SECRET"))
-    puts res.body
+    tokenRes = Net::HTTP.post_form(tokenUri, 'grant_type' => 'client_credentials', 'client_id' =>  ENV.fetch("PETFINDER_API_KEY"), 'client_secret' => ENV.fetch("PETFINDER_SECRET"))
+    token = ActiveSupport::JSON.decode(tokenRes.body)["access_token"]
+    puts token
 
     # Return JSON object
     render json: {'Fido': 'An ideal companion'}
