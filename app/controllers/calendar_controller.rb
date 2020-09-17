d3ee772882f9a1716f4ef8479f0cd1285a6c0ef0
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-17 13:33:02 -0400

    Cache results of Petfinder API for 1 hour

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,21 +6,22 @@ require 'petfinder'
 class CalendarController < ApplicationController
   def dogs
     allDogs = []
-    currentPage = 1
-    petfinder = Petfinder::Client.new(ENV.fetch("PETFINDER_API_KEY"), ENV.fetch("PETFINDER_SECRET"))
-    animals, pagination = petfinder.animals(type: 'dog', location: 'MA', page: currentPage, limit: 100)
-    allDogs.push(*animals)
-    
-    loop do
-      currentPage += 1
-      nextAnimal, nextPagination = petfinder.animals(type: 'dog', location: 'MA', page: currentPage, limit: 100)
-      allDogs.push(*nextAnimal)
-      if currentPage > pagination.total_pages
-        break
+    Rails.cache.fetch('cachedDogs', expires_in: 1.hours) do
+      currentPage = 1
+      petfinder = Petfinder::Client.new(ENV.fetch("PETFINDER_API_KEY"), ENV.fetch("PETFINDER_SECRET"))
+      animals, pagination = petfinder.animals(type: 'dog', location: 'MA', page: currentPage, limit: 100)
+      allDogs.push(*animals)
+      
+      loop do
+        currentPage += 1
+        nextAnimal, nextPagination = petfinder.animals(type: 'dog', location: 'MA', page: currentPage, limit: 100)
+        allDogs.push(*nextAnimal)
+        if currentPage > pagination.total_pages
+          break
+        end
       end
+      allDogs
     end
-
-    # Return JSON object
-    render json: allDogs
+    render json: Rails.cache.fetch('cachedDogs')
   end
 end
