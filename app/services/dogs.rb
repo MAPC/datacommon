02fd70f68e6578bdc2bdffd485e289ef2a7ea7cb
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-20 10:35:44 -0400

    Move endpoint logic from controller to service

---
+++ app/services/dogs.rb
@@ -0,0 +1,24 @@
+require 'petfinder'
+
+class Dogs
+  def initialize; end
+
+  def execute
+    all_dogs = []
+    Rails.cache.fetch('cachedDogs', expires_in: 1.hours) do
+      current_page = 1
+      petfinder = Petfinder::Client.new(ENV.fetch('PETFINDER_API_KEY'), ENV.fetch('PETFINDER_SECRET'))
+      animals, pagination = petfinder.animals(type: 'dog', location: 'MA', page: current_page, limit: 100)
+      all_dogs.push(*animals)
+
+      loop do
+        current_page += 1
+        next_animal, _next_pagination = petfinder.animals(type: 'dog', location: 'MA', page: current_page, limit: 100)
+        all_dogs.push(*next_animal)
+        break if current_page > pagination.total_pages
+      end
+      all_dogs
+    end
+    Rails.cache.fetch('cachedDogs')
+  end
+end
