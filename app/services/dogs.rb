721d0af8408534b6e260649f52aa2fda22446772
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-25 11:32:36 -0400

    Filter to adoptable dogs

--- app/services/dogs.rb
+++ app/services/dogs.rb
@@ -8,12 +8,12 @@ class Dogs
     Rails.cache.fetch('cachedDogs', expires_in: 1.hours) do
       current_page = 1
       petfinder = Petfinder::Client.new(Rails.application.credentials.petfinder[:api_key], Rails.application.credentials.petfinder[:secret])
-      animals, pagination = petfinder.animals(type: 'dog', location: 'MA', page: current_page, limit: 100)
+      animals, pagination = petfinder.animals(type: 'dog', location: 'MA', status: 'adoptable', page: current_page, limit: 100)
       all_dogs.push(*animals)
 
       loop do
         current_page += 1
-        next_animal, _next_pagination = petfinder.animals(type: 'dog', location: 'MA', page: current_page, limit: 100)
+        next_animal, _next_pagination = petfinder.animals(type: 'dog', location: 'MA', status: 'adoptable', page: current_page, limit: 100)
         all_dogs.push(*next_animal)
         break if current_page > pagination.total_pages
       end
