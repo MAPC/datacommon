2eb646824f42e97339d640fc834a7ae0cf1647c7
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-10-05 15:15:31 -0400

    Refactor header component into pure function

--- app/javascript/pages/components/partials/Header.jsx
+++ app/javascript/pages/components/partials/Header.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
-
 import logoImg from '../../assets/images/logo.svg';
 
-
-class Header extends React.Component {
-
-  render() {
-    const highlightCPLink = this.props.location.pathname.startsWith('/profile');
-
-    return (
-      <header className="container">
-        <nav>
-          <div className="scroll-wrapper">
-            <div className="header-brand">
-              <a href="/">
-                <img src={logoImg} alt="DataCommon Logo" />
-                DataCommon
-              </a>
-            </div>
-
-            <ul>
-              <li><a href="/browser">Datasets</a></li>
-              <li><a className={highlightCPLink ? 'active' : null} href="/#community-profiles">Community Profiles</a></li>
-              <li><a href="/gallery">Gallery</a></li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-    );
+function handleActivePage(subdirectory, link = '/home') {
+  if (subdirectory.startsWith(link)) {
+    return 'active';
+  } if (subdirectory.startsWith('/calendar') && link === '/gallery') {
+    return 'active';
   }
+  return null;
+}
 
-};
+const Header = ({ location }) => (
+  <header className="container">
+    <nav>
+      <div className="scroll-wrapper">
+        <div className="header-brand">
+          <a href="/">
+            <img src={logoImg} alt="DataCommon Logo" />
+            DataCommon
+          </a>
+        </div>
+
+        <ul>
+          <li><a className={handleActivePage(location.pathname, '/browser')} href="/browser">Datasets</a></li>
+          <li><a className={handleActivePage(location.pathname, '/profile')} href="/#community-profiles">Community Profiles</a></li>
+          <li><a className={handleActivePage(location.pathname, '/gallery')} href="/gallery">Gallery</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
 
 export default Header;
+export { handleActivePage };
