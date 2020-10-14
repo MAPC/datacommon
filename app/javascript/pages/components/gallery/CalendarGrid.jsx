4893bb2031f9fddb1491e368c29c898517e4869d
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-10-14 11:17:44 -0400

    Refactor and optimize calendar grid component

--- app/javascript/pages/components/gallery/CalendarGrid.jsx
+++ app/javascript/pages/components/gallery/CalendarGrid.jsx
@@ -1,46 +1,43 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import PropTypes from 'prop-types';
 import CalendarItem from './CalendarItem';
 import BlankCalendarItem from './BlankCalendarItem';
 import data from '../../assets/data/temp-cal-data.json';
 
-
-class CalendarGrid extends React.Component {
-  render() {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const existingCalendarItems = data.filter((item) => item.year === this.props.selectedYear)
-      .map((item, i) => {
-        months.shift();
-        return <CalendarItem
-          link={item.url}
-          month={item.month}
-          year={item.year}
-          image={item.image}
-          item={item}
-          title={item.title}
-          key={i}
-        />
-      });
-
-    // Create some number of blank cards
-    const blankCalendarItems = months.map((month, i) => {
-      const blankDisplayMonth = `${month} ${this.props.selectedYear}`;
-      const keyValue = `b${i}`;
+function populateGrid(selectedYear) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return months.map((currentMonth) => {
+    const monthData = data.find((item) => item.year === selectedYear && item.month === currentMonth);
+    if (monthData) {
       return (
-        <BlankCalendarItem
-          month={blankDisplayMonth}
-          key={keyValue}
+        <CalendarItem
+          link={monthData.url}
+          month={monthData.month}
+          year={monthData.year}
+          image={monthData.image}
+          item={monthData}
+          title={monthData.title}
+          key={`ci-${currentMonth}-${selectedYear}`}
         />
       );
-    });
-
-    const allCalendarItems = existingCalendarItems.concat(blankCalendarItems);
+    }
     return (
-      <ul className="CalendarGrid">
-        {allCalendarItems}
-      </ul>
+      <BlankCalendarItem
+        month={`${currentMonth} ${selectedYear}`}
+        key={`b-${currentMonth}-${selectedYear}`}
+      />
     );
-  }
+  });
 }
+
+const CalendarGrid = ({ selectedYear }) => (
+  <ul className="CalendarGrid">
+    {populateGrid(selectedYear)}
+  </ul>
+);
+
+CalendarGrid.propTypes = {
+  selectedYear: PropTypes.number.isRequired,
+};
 
 export default CalendarGrid;
