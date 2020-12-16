52a088f3634060295a32efa64895103888fe523e
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-12-16 17:19:35 -0500

    Update December w/Citation
    * Add Date and citiation for December.

--- app/javascript/pages/components/gallery/2020/December.jsx
+++ app/javascript/pages/components/gallery/2020/December.jsx
@@ -8,9 +8,11 @@ const colorPalette = ['#F3F3F3', '#B1C6D8', '#50789D', '#2e4b66', '#c1b9bb'];
 const colorPolygon = (value) => {
   if (value >= 200) {
     return colorPalette[3];
-  } if (value >= 100) {
+  }
+  if (value >= 100) {
     return colorPalette[2];
-  } if (value >= 50) {
+  }
+  if (value >= 50) {
     return colorPalette[1];
   }
   return colorPalette[0];
@@ -18,7 +20,8 @@ const colorPolygon = (value) => {
 const colorExpression = ['match', ['get', 'town']];
 
 function toCamelCase(muniName) {
-  return muniName.split(' ')
+  return muniName
+    .split(' ')
     .map((word) => word.charAt(0).concat(word.slice(1).toLowerCase()))
     .join(' ');
 }
@@ -27,13 +30,11 @@ function setHeader(currentMuni, medianObj) {
   if (currentMuni && medianObj[currentMuni] !== '-') {
     return (
       <>
-        <h3 className="calendar-viz__chart-title">{toCamelCase(currentMuni)}</h3>
+        <h3 className="calendar-viz__chart-title">
+          {toCamelCase(currentMuni)}
+        </h3>
         <h4 className="calendar-viz__chart-subtitle">
-          Median download speed:
-          {' '}
-          {d3.format('.2f')(medianObj[currentMuni])}
-          {' '}
-          Mbps
+          Median download speed: {d3.format('.2f')(medianObj[currentMuni])} Mbps
         </h4>
       </>
     );
@@ -41,13 +42,17 @@ function setHeader(currentMuni, medianObj) {
   if (currentMuni) {
     return (
       <>
-        <h3 className="calendar-viz__chart-title">{toCamelCase(currentMuni)}</h3>
+        <h3 className="calendar-viz__chart-title">
+          {toCamelCase(currentMuni)}
+        </h3>
         <h4 className="calendar-viz__chart-subtitle">Data unavailable</h4>
       </>
     );
   }
   return (
-    <h3 className="calendar-viz__chart-title">Click on a municipality to see the distribution of its speed test results</h3>
+    <h3 className="calendar-viz__chart-title">
+      Click on a municipality to see the distribution of its speed test results
+    </h3>
   );
 }
 
@@ -68,7 +73,8 @@ const December = () => {
     const decMap = new mapboxgl.Map({
       container: 'decMap',
       style: 'mapbox://styles/ihill/ckcnnn63u26o11ip2qf4odwyp',
-      accessToken: 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg',
+      accessToken:
+        'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg',
       center: [-70.944, 42.37],
       zoom: 8.4,
       minZoom: 6,
@@ -85,7 +91,12 @@ const December = () => {
     if (mapData) {
       const tempMedianObj = {};
       mapData.forEach((row) => {
-        colorExpression.push(row.muni, row.median_download_speed_mbps_2020 !== '-' ? colorPolygon(+row.median_download_speed_mbps_2020) : colorPalette[4]);
+        colorExpression.push(
+          row.muni,
+          row.median_download_speed_mbps_2020 !== '-'
+            ? colorPolygon(+row.median_download_speed_mbps_2020)
+            : colorPalette[4]
+        );
         tempMedianObj[row.muni] = row.median_download_speed_mbps_2020;
       });
       setMedianObj(tempMedianObj);
@@ -143,10 +154,18 @@ const December = () => {
 
   if (map) {
     map.on('click', (e) => {
-      const clickedData = map.queryRenderedFeatures([e.point.x, e.point.y], { layers: ['Median Download Speed'] });
+      const clickedData = map.queryRenderedFeatures([e.point.x, e.point.y], {
+        layers: ['Median Download Speed'],
+      });
       if (clickedData.length > 0) {
         setMuni(clickedData[0].properties.town);
-        map.setPaintProperty('Highlighted municipality', 'line-opacity', ['match', ['get', 'town'], clickedData[0].properties.town, 1, 0]);
+        map.setPaintProperty('Highlighted municipality', 'line-opacity', [
+          'match',
+          ['get', 'town'],
+          clickedData[0].properties.town,
+          1,
+          0,
+        ]);
       } else {
         setMuni('');
         map.setPaintProperty('Highlighted municipality', 'line-opacity', 0);
@@ -161,56 +180,190 @@ const December = () => {
           <div id="decMap" className="mapboxgl__container" />
           <aside className="legend__wrapper legend__wrapper--datacommon">
             <div className="legend">
-              <span className="legend__title legend__title--datacommon">Median Download Speed (Megabits per Second)</span>
+              <span className="legend__title legend__title--datacommon">
+                Median Download Speed (Megabits per Second)
+              </span>
               <svg height="200" width="168">
-                <rect x="2" y="2" width="16" height="16" fill="#F3F3F3" stroke="#231F20" />
-                <text x="28" y="14" className="legend__entry legend__entry--datacommon" fill="#231F20">1 – 50</text>
-                <rect x="2" y="30" width="16" height="16" fill="#B1C6D8" stroke="#231F20" />
-                <text x="28" y="42" className="legend__entry legend__entry--datacommon" fill="#231F20">50 – 100</text>
-                <rect x="2" y="58" width="16" height="16" fill="#50789D" stroke="#231F20" />
-                <text x="28" y="70" className="legend__entry legend__entry--datacommon" fill="#231F20">100 – 200</text>
-                <rect x="2" y="86" width="16" height="16" fill="#2e4b66" stroke="#231F20" />
-                <text x="28" y="98" className="legend__entry legend__entry--datacommon" fill="#231F20">200+</text>
-                <rect x="2" y="114" width="16" height="16" fill="#c1b9bb" stroke="#231F20" />
-                <text x="28" y="126" className="legend__entry legend__entry--datacommon" fill="#231F20">Data unavailable</text>
-                <line x1="2" y1="148" x2="18" y2="148" style={{ stroke: '#231F20', strokeWidth: 3.5 }} />
-                <text x="28" y="154" className="map__legend-entry" fill="#1F4E46">MAPC border</text>
-                <line x1="2" y1="176" x2="18" y2="176" style={{ stroke: '#FDB525', strokeWidth: 3.5 }} />
-                <text x="28" y="182" className="map__legend-entry" fill="#1F4E46">Selected</text>
-                <text x="28" y="194" className="map__legend-entry" fill="#1F4E46">municipality</text>
-
+                <rect
+                  x="2"
+                  y="2"
+                  width="16"
+                  height="16"
+                  fill="#F3F3F3"
+                  stroke="#231F20"
+                />
+                <text
+                  x="28"
+                  y="14"
+                  className="legend__entry legend__entry--datacommon"
+                  fill="#231F20"
+                >
+                  1 – 50
+                </text>
+                <rect
+                  x="2"
+                  y="30"
+                  width="16"
+                  height="16"
+                  fill="#B1C6D8"
+                  stroke="#231F20"
+                />
+                <text
+                  x="28"
+                  y="42"
+                  className="legend__entry legend__entry--datacommon"
+                  fill="#231F20"
+                >
+                  50 – 100
+                </text>
+                <rect
+                  x="2"
+                  y="58"
+                  width="16"
+                  height="16"
+                  fill="#50789D"
+                  stroke="#231F20"
+                />
+                <text
+                  x="28"
+                  y="70"
+                  className="legend__entry legend__entry--datacommon"
+                  fill="#231F20"
+                >
+                  100 – 200
+                </text>
+                <rect
+                  x="2"
+                  y="86"
+                  width="16"
+                  height="16"
+                  fill="#2e4b66"
+                  stroke="#231F20"
+                />
+                <text
+                  x="28"
+                  y="98"
+                  className="legend__entry legend__entry--datacommon"
+                  fill="#231F20"
+                >
+                  200+
+                </text>
+                <rect
+                  x="2"
+                  y="114"
+                  width="16"
+                  height="16"
+                  fill="#c1b9bb"
+                  stroke="#231F20"
+                />
+                <text
+                  x="28"
+                  y="126"
+                  className="legend__entry legend__entry--datacommon"
+                  fill="#231F20"
+                >
+                  Data unavailable
+                </text>
+                <line
+                  x1="2"
+                  y1="148"
+                  x2="18"
+                  y2="148"
+                  style={{ stroke: '#231F20', strokeWidth: 3.5 }}
+                />
+                <text
+                  x="28"
+                  y="154"
+                  className="map__legend-entry"
+                  fill="#1F4E46"
+                >
+                  MAPC border
+                </text>
+                <line
+                  x1="2"
+                  y1="176"
+                  x2="18"
+                  y2="176"
+                  style={{ stroke: '#FDB525', strokeWidth: 3.5 }}
+                />
+                <text
+                  x="28"
+                  y="182"
+                  className="map__legend-entry"
+                  fill="#1F4E46"
+                >
+                  Selected
+                </text>
+                <text
+                  x="28"
+                  y="194"
+                  className="map__legend-entry"
+                  fill="#1F4E46"
+                >
+                  municipality
+                </text>
               </svg>
-              <a href="https://speed.measurementlab.net/" className="map__legend-entry map__legend-entry--bold map__legend-link" fill="#1F4E46">Test your Internet speed</a>
+              <a
+                href="https://speed.measurementlab.net/"
+                className="map__legend-entry map__legend-entry--bold map__legend-link"
+                fill="#1F4E46"
+              >
+                Test your Internet speed
+              </a>
             </div>
           </aside>
         </div>
         <div className="calendar-viz__chart-wrapper">
-          { spec ? (
+          {spec ? (
             <>
-              { setHeader(currentMuni, medianObj) }
+              {setHeader(currentMuni, medianObj)}
               <VegaLite spec={spec} data={chartData[currentMuni]} />
             </>
-          ) : ''}
+          ) : (
+            ''
+          )}
         </div>
       </div>
+      <a
+        href="https://www.measurementlab.net/data/"
+        className="map__legend-entry map__legend-link"
+      >
+        Data from Measurement Lab from 1-1-2020 to 11-23-2020
+      </a>
       <p>
-        Fast, reliable internet is a basic need in the world of COVID. Not only does work, school, and healthcare often get done online, it often gets done online by multiple members of a household at once.
+        Fast, reliable internet is a basic need in the world of COVID. Not only
+        does work, school, and healthcare often get done online, it often gets
+        done online by multiple members of a household at once.
       </p>
       <p>
-        To reveal where there might be speed deserts – gaps in adequate internet access – MAPC mapped the median download speed for each municipality in the Commonwealth. We also charted, by city or town, the distribution of internet connections that perform at various rates of download speeds.
+        To reveal where there might be speed deserts – gaps in adequate internet
+        access – MAPC mapped the median download speed for each municipality in
+        the Commonwealth. We also charted, by city or town, the distribution of
+        internet connections that perform at various rates of download speeds.
       </p>
       <p>
-        Most Massachusetts communities have an average download speed of 50–100 megabits per second (mbps). But some average less than 30mbps – close to the threshold of what’s considered acceptable service by the federal government.
+        Most Massachusetts communities have an average download speed of 50–100
+        megabits per second (mbps). But some average less than 30mbps – close to
+        the threshold of what’s considered acceptable service by the federal
+        government.
       </p>
       <p>
-        Of course, many factors contribute to speed, from equipment performance to heavy use to multiple people sharing a connection. And download speed is not the only measure of internet access: That also requires an adequate device and digital literacy.
+        Of course, many factors contribute to speed, from equipment performance
+        to heavy use to multiple people sharing a connection. And download speed
+        is not the only measure of internet access: That also requires an
+        adequate device and digital literacy.
       </p>
       <p>
-        For a household already working to bridge the
-        {' '}
-        <a href="https://www.mapc.org/resource-library/digital-divide/" className="calendar-viz__link">Digital Divide</a>
-        {' '}
-        – already likely to have slower or fewer devices and to face barriers to internet fluency – low download speeds and poor connectivity can make things even harder.
+        For a household already working to bridge the{' '}
+        <a
+          href="https://www.mapc.org/resource-library/digital-divide/"
+          className="calendar-viz__link"
+        >
+          Digital Divide
+        </a>{' '}
+        – already likely to have slower or fewer devices and to face barriers to
+        internet fluency – low download speeds and poor connectivity can make
+        things even harder.
       </p>
       <p>MAPC recommends municipalities tackle speed deserts in three steps:</p>
       <ol className="calendar-viz__list calendar-viz__list--numbered">
@@ -222,18 +375,27 @@ const December = () => {
         <li>
           <strong>Assemble appropriate provider partners</strong>
           {' – '}
-          Gather entities capable of providing the main elements of access: connections, digital services, equipment, and ongoing maintenance. Depending on the community this may include entities such as housing authorities, healthcare providers, schools, library, IT support providers, workforce training programs, and others.
+          Gather entities capable of providing the main elements of access:
+          connections, digital services, equipment, and ongoing maintenance.
+          Depending on the community this may include entities such as housing
+          authorities, healthcare providers, schools, library, IT support
+          providers, workforce training programs, and others.
         </li>
         <li>
           <strong>Technology and Procurement</strong>
           {' – '}
-          Take inventory resources such as dark fiber lines, carrier hotels, and other components of internet infrastructure in order to determine needs.
+          Take inventory resources such as dark fiber lines, carrier hotels, and
+          other components of internet infrastructure in order to determine
+          needs.
         </li>
       </ol>
       <p>
-        For assistance in exploring issues related to the digital divide, contact
-        {' '}
-        <a href="mailto:jeichen@mapc.org" className="calendar-viz__link">MAPC Senior Economic Development Planner, Josh Eichen</a>.
+        For assistance in exploring issues related to the digital divide,
+        contact{' '}
+        <a href="mailto:jeichen@mapc.org" className="calendar-viz__link">
+          MAPC Senior Economic Development Planner, Josh Eichen
+        </a>
+        .
       </p>
     </>
   );
