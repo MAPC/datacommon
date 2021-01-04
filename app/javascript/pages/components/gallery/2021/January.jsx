3f294c70a49fc96a844485024c854eba05550bbf
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-01-04 08:34:45 -0500

    Fix link to download spatial data

--- app/javascript/pages/components/gallery/2021/January.jsx
+++ app/javascript/pages/components/gallery/2021/January.jsx
@@ -94,7 +94,7 @@ const January = () => {
                 <a className="map-legend__link" href="https://mapc365.sharepoint.com/:x:/s/DataServicesSP/Efonrnmw_kdMhmG3Dw2BkTcBIpe2sC_2ADWTWfUjOs4JhQ?e=K65BCE">Tabular data (.csv)</a>
               </li>
               <li className="map-legend__resource map-legend__entry">
-                <a className="map-legend__link" href="https://mapc365.sharepoint.com/sites/DataServicesSP/Shared%20Documents/Forms/AllItems.aspx?originalPath=aHR0cHM6Ly9tYXBjMzY1LnNoYXJlcG9pbnQuY29tLzpmOi9zL0RhdGFTZXJ2aWNlc1NQL0VyS2tYU0xIX2lCT2xEaEpyVFhsZHJZQklJWjRaWGU0Qmt3N095VmFwVnBYM1E%5FcnRpbWU9ekZlV3AzeW4yRWc&viewid=8aabc982%2D537d%2D48a6%2D89f7%2D8d8f0e9b716c&id=%2Fsites%2FDataServicesSP%2FShared%20Documents%2FZoning%20Database%2FShapefiles">Spatial data (.shp)</a>
+                <a className="map-legend__link" href="https://mapc365.sharepoint.com/:f:/s/DataServicesSP/ErKkXSLH_iBOlDhJrTXldrYBIIZ4ZXe4Bkw7OyVapVpX3Q?e=iRkWVB">Spatial data (.shp)</a>
               </li>
               <li className="map-legend__resource map-legend__entry">
                 <a className="map-legend__link" href="https://zoningatlas.mapc.org">MAPC Zoning Atlas</a>
