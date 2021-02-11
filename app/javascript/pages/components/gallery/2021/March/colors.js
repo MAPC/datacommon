211dd8fbbcc8e66cceaa26a342d03b0bd512fb0f
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-11 17:25:27 -0500

    Separate visualizations into components

---
+++ app/javascript/pages/components/gallery/2021/March/colors.js
@@ -0,0 +1,4 @@
+const participation = ['#0097C4', '#00613F', '#D59C29']; // More, no difference, less
+const scale = ['#111436', '#233069', '#3B66B0', '#67CBE4', '#92C9ED']; // 1 best, 2, 3, 4, 5 worst
+
+export { participation, scale };
