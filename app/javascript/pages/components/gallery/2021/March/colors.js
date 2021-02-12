4b4d9f1b59faa3d4f80c3d3cefac8dae3a7c4627
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-12 11:28:46 -0500

    Add draft narrative panels

--- app/javascript/pages/components/gallery/2021/March/colors.js
+++ app/javascript/pages/components/gallery/2021/March/colors.js
@@ -1,4 +1,4 @@
 const participation = ['#0097C4', '#00613F', '#D59C29']; // More, no difference, less
-const scale = ['#111436', '#233069', '#3B66B0', '#67CBE4', '#92C9ED']; // 1 best, 2, 3, 4, 5 worst
+const scale = ['#233069', '#0097C4', '#3B66B0', '#92C9ED', '#C4E7EB']; // 1 best, 2, 3, 4, 5 worst
 
 export { participation, scale };
