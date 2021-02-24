9d3c7cfd499440f4dfb7bb4768fbec3f9c86e6f7
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-24 14:49:34 -0500

    Adjust color scale, narratives

--- app/javascript/pages/components/gallery/2021/March/colors.js
+++ app/javascript/pages/components/gallery/2021/March/colors.js
@@ -1,4 +1,4 @@
 const participation = ['#0097C4', '#00613F', '#D59C29']; // More, no difference, less
-const scale = ['#233069', '#0097C4', '#3B66B0', '#92C9ED', '#C4E7EB']; // 1 best, 2, 3, 4, 5 worst
+const scale = ['#19244d', '#2e3f8a', '#4774c2', '#83abd8', '#cae5f7']; // 1 best, 2, 3, 4, 5 worst
 
 export { participation, scale };
