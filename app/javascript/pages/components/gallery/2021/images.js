061b39641e5af233b2b2a1727ec8f435775b8a26
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-25 13:41:19 -0500

    Adjust images for homepage and gallery

--- app/javascript/pages/components/gallery/2021/images.js
+++ app/javascript/pages/components/gallery/2021/images.js
@@ -1,5 +1,5 @@
 import January from '../../../assets/images/gallery/2021/january.png';
 import February from '../../../assets/images/gallery/2021/february.png';
-import March from '../../../assets/images/gallery/2021/february.png';
+import March from '../../../assets/images/gallery/2021/march.png';
 
 export { January, February, March };
