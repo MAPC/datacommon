9137f75078ced60adbdd05f3ba17dbc40329e118
Matthew Zagaja <mzagaja@alum.wpi.edu>
2021-04-21 17:42:24 -0400

    Update Picture + Text

--- app/javascript/pages/components/gallery/2021/May.jsx
+++ app/javascript/pages/components/gallery/2021/May.jsx
@@ -98,21 +98,18 @@ const May = () => {
     <>
       <h1 className="calendar-viz__title">COVID Vulnerability Index</h1>
       <div className="calendar-viz" />
+      
       <p>
-        Most of us forget about sewage as we flush. But how we dispose of wastewater has a profound influence on community growth, the economy, and the environment. Where there are no public sewers, the considerable land-area requirements of on-site wastewater treatment limits development potential. This contrasts with areas served by the Massachusetts Water Resources Authority (MWRA), which sends wastewater to Deer Island for processing. Indeed, the availability of sewers shapes land-use patterns in Metro Boston.
+        MAPC has created a COVID-19 Recovery Need Score to better identify where municipalities stand in relation to each other on factors important to the need for COVID-19 recovery support. According to this analysis, Chelsea scores higher (more in need of funding support) than all of the Entitlement communities except for Lawrence.
       </p>
       <p>
-        For this reason, sewer system data was on MAPC&apos;s 2008 &quot;Most Wanted Datasets&quot; list. We have been slowly compiling the data ever since, focusing on the locations of existing wastewater collection pipes.
+        The score is measured across three dimensions (sub-scores), which include data as follows:
       </p>
-      <p>
-        As one might expect with information kept at the local level, data about pipe locations are stored in a variety of different formats and include a variety of information. Building upon work previously conducted by MassGIS, MAPC defined a basic data standard. We collected information from municipalities, from the U.S. Geological Survey, and from the MWRA. Unfortunately, some municipalities didn&apos;t share their data with us, and some of the data provided is incomplete or may now be out of date.
-      </p>
-      <p>
-        Despite these limitations, the resulting dataset can be a valuable resource for planners and policy makers. For example, a new state law requires all MBTA-served communities to zone for multifamily housing. This sewer-system information will help identify the most promising sites for such zoning. MAPC has used this information to evaluate redevelopment potential at commercial sites across the region as part of a forthcoming &quot;Retrofitting Suburbia&quot; research report, and it is currently doing an analysis of wastewater and water supply capacity in the South Shore subregion.
-      </p>
-      <p>
-        The lack of centralized sewer need not completely preclude denser development: modern &quot;package treatment,&quot; or other small-scale wastewater treatment facilities, can serve a large development or district while also protecting water quality. These are typically privately-owned facilities, but they could also be owned and operated by a municipality or public wastewater district. The feasibility, capacity, and cost of such facilities is influenced by site specific soil and hydrology as well as state and local regulations.
-      </p>
+      <ul>
+        <li>COVID Impact Sub-score – COVID-19 case rates and average unemployment rate from February 2020 to January 2021; this score is weighted x1.5 in the final Recovery Need Score.</li>
+        <li>Income Sub-score - poverty rate, percent low-income population; this score is weighted x1 in the final Recovery Need Score.</li>
+        <li> Demographic Score - percent people of color, and percent born outside the United States; this score is weighted x1 in the final Recovery Need Score.</li>
+      </ul>
     </>
   );
 };
