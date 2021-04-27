2d2da591270f60f635a950150821b2726e7e2f3a
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-04-27 12:31:58 -0400

    Remove Boston outlier, start on legend

--- app/javascript/pages/components/gallery/2021/May.jsx
+++ app/javascript/pages/components/gallery/2021/May.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as d3 from 'd3';
 
+function tooltipLeft(event, tooltip) {
+  return `${event.clientX - tooltip.offsetWidth - 40}px`;
+}
+
+function tooltipTop(event, tooltip) {
+  return `${event.pageY - tooltip.offsetHeight - 140}px`;
+}
+
+const margin = {
+  top: 10, right: 20, bottom: 40, left: 100,
+};
+
+const bubbleColors = {
+  TRUE: '#69b3a2',
+  FALSE: '#2c4d46',
+};
+
 const May = () => {
-  d3.csv('/assets/arp_scoring_entitlement_communities_aid.csv').then((response) => {
-    const margin = {
-      top: 10, right: 20, bottom: 40, left: 100,
-    };
-    const width = getComputedStyle(document.querySelector('.calendar-viz')).width.slice(0, -2);
+  const [data, setData] = useState();
+  useEffect(() => {
+    d3.csv('/assets/arp_scoring_entitlement_communities_aid.csv').then((response) => {
+      setData(response);
+    });
+  }, []);
+  if (data) {
     const height = 700;
-    const bubbleColors = {
-      TRUE: '#69b3a2',
-      FALSE: '#2c4d46',
-    };
-    const svg = d3.select('.calendar-viz')
-      .append('svg')
+    const width = getComputedStyle(document.querySelector('.calendar-viz')).width.slice(0, -2);
+    const svg = d3.select('.calendar-viz__chart')
       .attr('width', width - margin.left - margin.right)
       .attr('height', height + margin.top + margin.bottom)
+      .attr('class', 'calendar-viz__svg')
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
@@ -24,21 +40,12 @@ const May = () => {
       .style('opacity', 0)
       .attr('class', 'tooltip');
 
-    const tooltipHtml = (municipality) => {
-      let tooltipDetails = `<p class='tooltip__title'>${municipality.municipal}</p>
-      <ul class='tooltip__list'>
-      <li class='tooltip__text'>Popluation: ${municipality.Population}</li>`;
-      tooltipDetails += '</ul>';
-      return tooltipDetails;
-    };
-
-    function tooltipLeft(event, tooltip) {
-      return `${event.clientX - tooltip.offsetWidth - 40}px`;
-    }
-
-    function tooltipTop(event, tooltip) {
-      return `${event.pageY - tooltip.offsetHeight - 140}px`;
-    }
+    const tooltipHtml = (municipality) => `
+        <p class='tooltip__title'>${municipality.municipal}</p>
+        <ul class='tooltip__list'>
+          <li class='tooltip__text'>Popluation: ${d3.format(',')(municipality.Population)}</li>
+          <li class='tooltip__text'>${municipality.entitlement === 'TRUE' ? 'Entitlement community' : 'Not an entitlement community'}</li>
+        </ul>`;
 
     const x = d3.scaleLinear()
       .domain([0, 1.15])
@@ -48,7 +55,7 @@ const May = () => {
       .call(d3.axisBottom(x));
 
     const y = d3.scaleLinear()
-      .domain([0, 570000000])
+      .domain([0, 160000000])
       .range([height, 0]);
     svg.append('g')
       .call(d3.axisLeft(y));
@@ -59,7 +66,7 @@ const May = () => {
 
     svg.append('g')
       .selectAll('dot')
-      .data(response)
+      .data(data)
       .enter()
       .append('circle')
       .attr('cx', (d) => x(d['COVID Impact Score']))
@@ -96,13 +103,65 @@ const May = () => {
       .style('text-anchor', 'middle')
       .style('font-family', 'Montserrat, sans-serif')
       .text('Allocated Aid (US Dollars)');
-  });
+  }
 
   return (
     <>
       <h1 className="calendar-viz__title">COVID Vulnerability Index</h1>
-      <div className="calendar-viz" />
-      
+      <div className="calendar-viz">
+        <svg className="calendar-viz__chart" />
+        <div
+          className="calendar-viz__legend"
+          style={{
+            position: 'absolute', top: '0', right: '120px', background: '#F0EFE7', padding: '8px',
+          }}
+        >
+          <svg className="calendar-viz__legend" height="120" width="168">
+            <text
+              x="0"
+              y="10"
+              className="d3-map__legend-entry d3-map__legend-entry--bold"
+              fill="#1F4E46"
+            >
+              Municipality Type
+            </text>
+            <circle fill={bubbleColors.TRUE} opacity="0.6" cx="10" cy="30" r="6" width="25" height="10" />
+            <text
+              x="24"
+              y="33"
+              className="d3-map__legend-entry"
+              fill="#1F4E46"
+            >
+              Entitlement
+            </text>
+            <text
+              x="24"
+              y="50"
+              className="d3-map__legend-entry"
+              fill="#1F4E46"
+            >
+              community
+            </text>
+            <circle fill={bubbleColors.FALSE} opacity="0.6" cx="10" cy="70" r="6" width="25" height="10" />
+            <text
+              x="24"
+              y="73"
+              className="d3-map__legend-entry"
+              fill="#1F4E46"
+            >
+              Not an entitlement
+            </text>
+            <text
+              x="24"
+              y="90"
+              className="d3-map__legend-entry"
+              fill="#1F4E46"
+            >
+              community
+            </text>
+          </svg>
+        </div>
+      </div>
       <p>
         MAPC has created a COVID-19 Recovery Need Score to better identify where municipalities stand in relation to each other on factors important to the need for COVID-19 recovery support. According to this analysis, Chelsea scores higher (more in need of funding support) than all of the Entitlement communities except for Lawrence.
       </p>
