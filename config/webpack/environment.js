ebb541e07312080ed24468216dcbaa33cc90a84b
Jonathan McKenzie <jmckenzie@mapc.org>
2025-01-08 14:41:32 -0500

    Move webpack updates to common environment file

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,24 @@ const { environment } = require('@rails/webpacker')
 
 environment.loaders.delete('nodeModules');
 
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
+
 module.exports = environment;
